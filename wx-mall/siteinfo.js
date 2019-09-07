a6283919e907516954cb61f9cd1b6fa9723dbfbb
jiewolf <jie1wolf@sina.com>
2019-09-07 17:21:38 +0800

    添加原始小程序代码
    购买了一个小程序代码算第一小程序商城代码吧

---
+++ wx-mall/siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  acid: -1,
+  version: "1.0.0",
+  siteroot: "http://www.abc.com",
+  apiroot: "https://xcx.topvip111.com/web/index.php?store_id=2&r=api/"
+  //微猫提示  只需要改域名和storeid即可
+  //独立版：https://hj.weixin2015.cn/web/index.php?store_id=1&r=api/
+  //微擎版：https://www.weixin2015.cn/addons/zjhj_mall/core/web/index.php?store_id=1&r=api/
+};
