1476f61e00bb20ad8ad5311139eee7bdccb287ea
softServer <jie1wolf@sina.com>
2019-09-15 22:50:12 +0800

    修改小程序文件系统类型

--- wx-mall/siteinfo.js
+++ wx-mall/siteinfo.js
@@ -2,7 +2,7 @@ module.exports = {
   acid: -1,
   version: "1.0.0",
   siteroot: "http://www.abc.com",
-  apiroot: "https://xcx.topvip111.com/web/index.php?store_id=2&r=api/"
+  apiroot: "http://192.168.2.103/web/index.php?store_id=1&r=api/"
   //微猫提示  只需要改域名和storeid即可
   //独立版：https://hj.weixin2015.cn/web/index.php?store_id=1&r=api/
   //微擎版：https://www.weixin2015.cn/addons/zjhj_mall/core/web/index.php?store_id=1&r=api/
